b803eb03d9d7c5c99622c673421abf15ef5b985e
Dolores Polito <dolores.polito@gmail.com>
2024-05-22 19:07:16 -0300

    delete sitemap files

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    siteUrl: 'https://inesmiguensphoto.com', // Reemplaza con tu dominio
-    generateRobotsTxt: true,
-     // (Opcional) Genera robots.txt, es útil para SEO
-     sitemapSize: 9000, 
-  }
